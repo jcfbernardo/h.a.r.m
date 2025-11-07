9a9aabdbdb5de3b5a21567870acffff76d0ff862
João Carlos Fernandes Bernardo <joaocfb.dev@gmail.com>
2025-11-07 09:11:40 -0300

    feat: initial config

---
+++ harm_api/src/test/java/com/harm/api/harm/HarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harm.api.harm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
